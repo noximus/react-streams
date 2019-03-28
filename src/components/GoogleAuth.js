490cb0a05b1447e88f18d7b0368a169213cc5507
Henry Richardson <hrichardson83@gmail.com>
2019-03-27 21:57:24 -0400

    google login working

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -14,25 +14,36 @@ class GoogleAuth extends Component {
         this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
-  }
+  };
 
   onAuthChange = () => {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
-  }
+  };
+
+  onSignIn = () => {
+    console.log('clicked');
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+  
+  
 
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button className="ui red google button">
+        <button onClick={this.onSignOut} className="ui red google button">
           <i className="google icon" />
           Sign Out
         </button>
       )
     } else {
       return (
-        <button className="ui red google button">
+        <button onClick={this.onSignIn} className="ui red google button">
           <i className="google icon" />
           Sign In with Google
         </button>
