d3065a02c215225f6d99f5ecd497d76e86ed4bb9
Henry Richardson <hrichardson83@gmail.com>
2019-03-27 22:56:32 -0400

    auth with redux complete

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,14 +1,15 @@
 import { SIGN_IN, SIGN_OUT } from "../actions/types";
 
 const initialState = {
-  isSignedIn: null
+  isSignedIn: null,
+  userId: null
 };
 
-export default (state = initialState, { type }) => {
+export default (state = initialState, { type, payload }) => {
   switch (type) {
 
   case SIGN_IN:
-    return { ...state, isSignedIn: true }
+    return { ...state, isSignedIn: true, userId: payload }
 
   case SIGN_OUT:
     return { ...state, isSignedIn: false }
